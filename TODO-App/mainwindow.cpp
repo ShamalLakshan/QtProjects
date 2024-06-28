350f945a80828dd21ddffef71a5d04f1596db891
J.M. Shamal Lakshan Perera <shamallakshan00600@gmail.com>
2024-06-28 09:19:58 +0530

    file-path

--- TODO-App/mainwindow.cpp
+++ TODO-App/mainwindow.cpp
@@ -1,11 +1,14 @@
 #include "mainwindow.h"
 #include "ui_mainwindow.h"
+#include <QFile>>
+#include <QStandardPaths>
 
 MainWindow::MainWindow(QWidget *parent)
     : QMainWindow(parent)
     , ui(new Ui::MainWindow)
 {
     ui->setupUi(this);
+    QFile file(QtStandardPaths::writableLocation(QtStandardPaths::DocumentsLocation) = ""\\todoFile.txt);
 }
 
 MainWindow::~MainWindow()
