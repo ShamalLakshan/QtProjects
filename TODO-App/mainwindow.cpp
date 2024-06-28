916eb0003968a3ce9326fe8ca9999a79d7bff899
J.M. Shamal Lakshan Perera <shamallakshan00600@gmail.com>
2024-06-28 09:36:20 +0530

    read-file

--- TODO-App/mainwindow.cpp
+++ TODO-App/mainwindow.cpp
@@ -1,19 +1,49 @@
 #include "mainwindow.h"
 #include "ui_mainwindow.h"
-#include <QFile>>
+#include <QFile>
 #include <QStandardPaths>
+#include <QMessageBox>
 
 MainWindow::MainWindow(QWidget *parent)
     : QMainWindow(parent)
     , ui(new Ui::MainWindow)
 {
     ui->setupUi(this);
-    QFile file(QtStandardPaths::writableLocation(QtStandardPaths::DocumentsLocation) = ""\\todoFile.txt);
+
+    QFile file(QStandardPaths::writableLocation(QStandardPaths::DocumentsLocation) = "\\todoFile.txt");
+
+    if(!file.open(QIODevice::ReadWrite)){
+        QMessageBox::information(0, "error", file.errorString());
+    }
+
+    QTextStream in(&file);
+
+    while (!in.atEnd()) {
+        QListWidgetItem* item = new QListWidgetItem(in.readLine(), ui -> lstTasks);
+        ui -> lstTasks -> addItem(item);
+        item -> setFlags(item -> flags() | Qt::ItemIsEditable);
+    }
+    file.close();
 }
 
 MainWindow::~MainWindow()
 {
     delete ui;
+
+    QFile file(QStandardPaths::writableLocation(QStandardPaths::DocumentsLocation) = "\\todoFile.txt");
+
+    if(!file.open(QIODevice::ReadWrite)){
+        QMessageBox::information(0, "error", file.errorString());
+    }
+
+    QTextStream in(&file);
+
+    while (!in.atEnd()) {
+        QListWidgetItem* item = new QListWidgetItem(in.readLine(), ui -> lstTasks);
+        ui -> lstTasks -> addItem(item);
+        item -> setFlags(item -> flags() | Qt::ItemIsEditable);
+    }
+    file.close();
 }
 
 void MainWindow::on_btnAdd_clicked()
