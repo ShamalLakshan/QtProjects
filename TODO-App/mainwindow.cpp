09f3a896925fe1fc3903ebb15f7b32c29d002a0d
J.M. Shamal Lakshan Perera <shamallakshan00600@gmail.com>
2024-06-28 08:11:24 +0530

    add-task

--- TODO-App/mainwindow.cpp
+++ TODO-App/mainwindow.cpp
@@ -12,3 +12,25 @@ MainWindow::~MainWindow()
 {
     delete ui;
 }
+
+void MainWindow::on_btnAdd_clicked()
+{
+    QListWidgetItem* item = new QListWidgetItem(ui-> txtTask -> text(), ui-> lstTasks);
+    ui-> lstTasks->addItem(item);
+    item->setFlags(item -> flags() | Qt::ItemIsEditable);
+    ui->txtTask->clear();
+    ui->txtTask->setFocus();
+}
+
+
+void MainWindow::on_btnRemove_clicked()
+{
+
+}
+
+
+void MainWindow::on_btnRemoveAll_clicked()
+{
+
+}
+
